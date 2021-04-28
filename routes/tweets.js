16dd41f5840ccd9e5a9c8c1a616866d1a5d9e5ab
Eddy <edkim220@gmail.com>
2021-04-28 16:10:38 -0400

    phase 2 completed

---
+++ routes/tweets.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "test tweets index" });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
